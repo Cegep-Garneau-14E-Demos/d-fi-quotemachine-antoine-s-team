ed42090f82ea912518835ed581ec6331d2f82881
comeonwindows11 <comeonwindows@mail.com>
2026-04-20 13:50:36 -0400

    feat(AddQuote): Backend du addQuote implémenté

--- QuoteMachine-ExerciceGit/QuoteManager.cs
+++ QuoteMachine-ExerciceGit/QuoteManager.cs
@@ -32,10 +32,10 @@ namespace QuoteMachine_ExerciceGit
         public void AddQuote(string text, string author)
         {
             //Avant de commencer, décommenter le test suivant:
-            //AddQuote_ShouldIncreaseQuoteCount
+            //AddQuote_ShouldIncreaseQuoteCount();
 
             //Avant de créer votre PR, faites un git rebase sur main pour vous assurer que vous avez la dernière version du code.
-            throw new NotImplementedException("À implémenter dans feature/add-quote");
+            _quotes.Add(new Quote { Text = text, Author = author });
         }
 
         public void SaveToCSVFile(string path)
